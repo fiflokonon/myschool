a8fc23410c687a8febc1b5550132a5c5b72fcd67
fifonsi <fiflokonon@gmail.com>
2023-01-20 21:38:46 +0100

    routes-part1-pro

--- config/routes.php
+++ config/routes.php
@@ -3,14 +3,16 @@
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
+use App\Domain\Core\Middleware\CorsMiddleware;
+use App\Domain\Core\Middleware\LoginMiddleware;
 
 use App\Action\User\UsersAction;
 use App\Action\User\UserAction;
 use App\Action\User\DeleteUserAction;
 use App\Action\User\CreateUserAction;
 use App\Action\User\LoginAction;
-use App\Domain\Core\Middleware\CorsMiddleware;
-use App\Domain\Core\Middleware\LoginMiddleware;
+
 
 use App\Action\School\SchoolsAction;
 use App\Action\School\SchoolAction;
@@ -22,6 +24,7 @@ use App\Action\Motif\MotifsAction;
 use App\Action\Motif\DeleteMotifAction;
 use App\Action\Motif\SchoolMotifsAction;
 use App\Action\Motif\DeleteSchoolMotifsAction;
+use App\Action\Motif\CreateMotifAction;
 
 use App\Action\Event\EventsAction;
 use App\Action\Event\EventAction;
@@ -37,17 +40,34 @@ return function (App $app) {
         return $response;
     });
 
-    /***********************************************
-     *              ROUTES WITHOUT AUTH            *
-     **********************************************/
+    /***************************
+     *     ROUTES             *
+     *************************/
+
+    /** ROUTES NO REQUIRE AUTH **/
     $app->post('/login', LoginAction::class);
     $app->post('signup', CreateUserAction::class);
 
+    /*** ROUTES REQUIRED AUTH ********/
+    $app->group('/api', function (RouteCollectorProxy $app)
+    {
+        $app->get('/users', UsersAction::class);
+        $app->get('/users/{id}', UserAction::class);
+        $app->delete('/users/{id}', DeleteUserAction::class);
+        #$app->post('/users', CreateUserAction::class);
 
-    $app->get('/users', UsersAction::class);
-    $app->get('/users/{id}', UserAction::class);
-    $app->delete('/users/{id}', DeleteUserAction::class);
-    $app->post('/users', CreateUserAction::class);
+        $app->get('/schools', SchoolsAction::class);
+        $app->get('/schools/{id}', SchoolAction::class);
+        $app->post('/schools', CreateSchoolAction::class);
+        $app->delete('/schools/{id}', DeleteSchoolAction::class);
+
+        $app->get('/motifs', MotifsAction::class);
+        $app->get('/motifs/{id}', MotifAction::class);
+        $app->get('/schools/{id}/motifs', SchoolMotifsAction::class);
+        $app->post('/schools/{id}/motifs', CreateMotifAction::class);
+        $app->delete('/motifs/{id}', DeleteMotifAction::class);
+
+    });
 
 
 
