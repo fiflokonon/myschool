fd81c7aacc7525895f28d837161caea2009e3fa9
fifonsi <fiflokonon@gmail.com>
2023-02-11 13:47:06 +0100

    del-dd

--- src/Domain/User/Service/UsersService.php
+++ src/Domain/User/Service/UsersService.php
@@ -25,7 +25,6 @@ final class UsersService
     public function getUsers()
     {
         $users = $this->repository->listUsers();
-        die(var_dump($users));
         if (empty($users))
             return [
                 "success" => false,
